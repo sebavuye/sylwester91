d8b068089f265379e307fbffffed67ca3b3c4c87
sebastianvuye <sebastian.vuye@gmail.com>
2023-04-26 21:51:26 +0200

    chore: add custom variable fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
+import { Cinzel, Josefin_Sans } from "next/font/google";
 import type { AppProps } from "next/app";
 
+const josefinSans = Josefin_Sans({
+  variable: "--font-josefin-sans",
+  subsets: ["latin"],
+});
+
+const cinzel = Cinzel({
+  variable: "--font-cinzel",
+  subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${josefinSans.variable} ${cinzel.variable}`}>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <main>Test</main>;
+  return (
+    <>
+      <header className="col-start-2 row-start-2 flex flex-col items-center justify-center">
+        <h1 className="font-sans text-6xl uppercase leading-none font-normal">
+          Sylwester91
+        </h1>
+        <p className="font-serif text-sm leading-none">
+          Shots by Sebastian Vuye
+        </p>
+      </header>
+
+      <main className="col-start-2 row-start-3">
+      </main>
+    </>
+  );
 }
