52fa0c6c7cd3308b3a65711f3db96549757c68dc
sebastianvuye <sebastian.vuye@gmail.com>
2023-04-27 10:13:17 +0200

    feat: add netlify function for cloudinary

--- netlify/functions/cloudinary.ts
+++ netlify/functions/cloudinary.ts
@@ -1,12 +1,21 @@
-import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+const cloudinary = require('cloudinary').v2;
+import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
 
-const handler: Handler = async (
-  event: HandlerEvent,
-  context: HandlerContext
-) => {
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+  secure: true
+});
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  const file = event.body;
+
+  const res = await cloudinary.api.resources_by_tag('sylwester91', { context: true });
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify(res)
   };
 };
 
