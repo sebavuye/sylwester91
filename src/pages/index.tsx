003a0fb5a653418de84cf84e6d588345a95ff1a2
sebastianvuye <sebastian.vuye@gmail.com>
2023-04-28 21:49:46 +0200

    feat: add mobile layout changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,15 +7,15 @@ export default function Home({ data }: InferGetServerSidePropsType<typeof getSta
   return (
     <>
       <header className='col-start-2 row-start-2 flex flex-col items-center justify-center'>
-        <h1 className='font-sans text-6xl uppercase leading-none font-normal'>Sylwester91</h1>
+        <h1 className='font-sans text-4xl sm:text-6xl uppercase leading-none font-normal'>Sylwester91</h1>
         <p className='font-serif text-sm leading-none'>Shots by Sebastian Vuye</p>
       </header>
 
-      <main className='col-start-2 row-start-3 grid gap-10'>
+      <main className='col-start-2 row-start-3 grid gap-5 md:gap-10'>
         {data.resources.map(image => {
           if (Array.isArray(image)) {
             return (
-              <div key={image[0].asset_id} className='grid grid-cols-2 gap-10'>
+              <div key={image[0].asset_id} className='grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10'>
                 {image.map(subImage => (
                   <Image key={subImage.asset_id} alt={subImage.context?.custom?.alt ?? ''} publicId={subImage.public_id} />
                 ))}
