8ec1b8b490b9cec776aafa57ef8a8ffbc502fc5a
sebastianvuye <sebastian.vuye@gmail.com>
2023-04-29 13:33:51 +0200

    feat: add ultra-wide responsiveness to elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,8 +31,8 @@ export default function Home({ data }: InferGetServerSidePropsType<typeof getSta
   return (
     <>
       <header className='col-start-2 row-start-2 flex flex-col items-center justify-center'>
-        <h1 className='font-sans text-4xl sm:text-6xl uppercase leading-none font-normal'>Sylwester91</h1>
-        <p className='font-serif text-sm leading-none'>Shots by Sebastian Vuye</p>
+        <h1 className='font-sans text-4xl sm:text-6xl uppercase leading-none font-normal 3xl:text-7xl 4xl:text-8xl'>Sylwester91</h1>
+        <p className='font-serif text-sm leading-none 3xl:text-lg 4xl:text-xl'>Shots by Sebastian Vuye</p>
       </header>
 
       <main className='col-start-2 row-start-3 grid gap-5 lg:gap-10'>
@@ -51,22 +51,28 @@ export default function Home({ data }: InferGetServerSidePropsType<typeof getSta
         {isScrollToTopVisible ? (
           <button
             type='button'
-            className='fixed -rotate-90 right-12 bottom-12 md:bottom-6 text-white border bg-zinc-700 border-zinc-700 hover:bg-zinc-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded-full text-sm p-5 text-center inline-flex items-center'
+            className='fixed -rotate-90 right-12 bottom-12 md:bottom-6 text-white border bg-zinc-700 border-zinc-700 hover:bg-zinc-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-zinc-300 font-medium rounded-full text-sm p-5 4xl:p-10 text-center inline-flex items-center'
             onClick={handleScrollToTopClick}>
-            <svg aria-hidden='true' className='w-5 h-5' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+            <svg
+              aria-hidden='true'
+              className='w-5 h-5 3xl:w-7 3xl:h-7 4xl:w-10 4xl:h-10'
+              fill='currentColor'
+              viewBox='0 0 20 20'
+              xmlns='http://www.w3.org/2000/svg'>
               <path
                 fillRule='evenodd'
                 d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
                 clipRule='evenodd'
               />
             </svg>
+            {/* TODO */}
             <span className='sr-only'>Icon description</span>
           </button>
         ) : null}
       </main>
 
       <footer className='col-start-2 row-start-4 flex justify-center items-center'>
-        <p className='font-serif text-xs'>&copy; Sebastian Vuye {new Date().getFullYear()}</p>
+        <p className='font-serif text-xs 3xl:text-lg 4xl:text-xl'>&copy; Sebastian Vuye {new Date().getFullYear()}</p>
       </footer>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
       fontFamily: {
-        sans: ["var(--font-josefin-sans)", ...fontFamily.sans],
-        serif: ["var(--font-cinzel)", ...fontFamily.sans],
+        sans: ['var(--font-josefin-sans)', ...fontFamily.sans],
+        serif: ['var(--font-cinzel)', ...fontFamily.sans]
       },
-    },
+      screens: {
+        '3xl': '2560px',
+        '4xl': '3840px'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
